fbf66473cf21d6c22650e074ecd699f90c66b892
Alexhl3 <4l3j4ndr0.c1@gmail.com>
2020-10-14 15:27:48 -0300

    Cracion de modelo comentario

--- test1BlogUnab/blogunabtest/blogs/models.py
+++ test1BlogUnab/blogunabtest/blogs/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 from ckeditor.fields import RichTextField
-#antes de la db
-#constructor con atributos
+# antes de la db
+# constructor con atributos
+
 
 class Blog(models.Model):
-    titulo=models.CharField(max_length=35)
-    contenido= RichTextField()
-    #fecha=models.datetime.datetime.now()
-    blogs=models.Manager()
+    titulo = models.CharField(max_length=35)
+    contenido = RichTextField()
+    # fecha=models.datetime.datetime.now()
+    blogs = models.Manager()
 
     def __str__(self):
         return "{}".format(self.titulo)
 
-    
-    def getBlog(self,titulo):
+    def getBlog(self, titulo):
         for blog in self.blogs:
-            if blog.titulo==titulo:
+            if blog.titulo == titulo:
                 return blog
-        return None       
+        return None
 
 
-
-
+class Comentario(models.Model):
+    autor = models.CharField(max_length=35)
+    contenido = RichTextField()
+    # author
+    comentarios = models.Manager()
